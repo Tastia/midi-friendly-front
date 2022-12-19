c70caa9a0f399de437e5678aefe683e9298e59e8
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-12-19 14:24:11 +0100

    chore: clear v1 app code

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo docker container prune -f
-docker volume prune -f
-sudo docker-compose up -d --build
-sudo nginx -s reload
--- src/utils/env.ts
+++
@@ -1,2 +0,0 @@
-export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-export const apiSignUp = process.env.NEXT_PUBLIC_API_SIGNUP;
