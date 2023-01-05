6738d60055e9fb98ae863ca4844e5634dd2fee7b
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2023-01-05 20:02:21 +0100

    feat: refactor header buttons (move to NBtn)

--- src/components/Layout/OnboardingTrigger.vue
+++ src/components/Layout/OnboardingTrigger.vue
@@ -2,14 +2,13 @@
   <NTooltip>
     Lancer l'introduction
     <template #trigger>
-      <div
-        class="h-8 w-8 grid place-items-center rounded-lg cursor-pointer bg-gray-200 hover:bg-gray-300 overflow:hidden dark:(bg-gray-700/25 hover:bg-gray-600/25 overflow-hidden) transition-all ease-in-out duration-75"
-        @click="ShowOnboarding"
-      >
-        <i:material-symbols:question-mark
-          class="transition-all ease-in-out duration-150 transform"
-        />
-      </div>
+      <NButton size="small" secondary circle @click="ShowOnboarding">
+        <template #icon>
+          <i:material-symbols:question-mark
+            class="transition-all ease-in-out duration-150 transform"
+          />
+        </template>
+      </NButton>
     </template>
   </NTooltip>
 </template>
--- src/components/Layout/ToggleTheme.vue
+++ src/components/Layout/ToggleTheme.vue
@@ -1,23 +1,21 @@
 <template>
-  <div
-    class="h-8 w-8 grid place-items-center rounded-lg cursor-pointer bg-gray-200 hover:bg-gray-300 overflow:hidden dark:(bg-gray-700/25 hover:bg-gray-600/25 overflow-hidden) transition-all ease-in-out duration-75"
-    :class="buttonClass"
-    @click="appStore.toggleTheme()"
-  >
-    <Transition
-      :name="appStore.isDark ? 'slide-fade' : 'slide-fade-reverse'"
-      mode="out-in"
-    >
-      <i:mdi-white-balance-sunny
-        v-if="appStore.isDark"
-        class="transition-all ease-in-out duration-150 transform"
-      />
-      <i:ic-baseline-dark-mode
-        v-else
-        class="transition-all ease-in-out duration-150 transform"
-      />
-    </Transition>
-  </div>
+  <NButton size="small" secondary circle @click="appStore.toggleTheme">
+    <template #icon>
+      <Transition
+        :name="appStore.isDark ? 'slide-fade' : 'slide-fade-reverse'"
+        mode="out-in"
+      >
+        <i:mdi-white-balance-sunny
+          v-if="appStore.isDark"
+          class="transition-all ease-in-out duration-150 transform"
+        />
+        <i:ic-baseline-dark-mode
+          v-else
+          class="transition-all ease-in-out duration-150 transform"
+        />
+      </Transition>
+    </template>
+  </NButton>
 </template>
 
 <script setup lang="ts">
