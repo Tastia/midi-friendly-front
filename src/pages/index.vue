3eece8a8339b607e42e5e7923ceb7fd19204cd57
Mathis Angibaud <mathisangibaud@macbook-pro-de-mathis.home>
2023-01-25 18:37:09 +0100

    feat(homepage): add join-us CTA

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -36,6 +36,24 @@
     </div>
     <div class="w-full h-5/6 lg:w-1/2 image-container rounded-md"></div>
   </div>
+
+  <div class="p-18 join-us">
+    <div class="flex bg-slate-100 border border-black/10 rounded-md relative overflow-hidden">
+      <div class="flex flex-col w-3/5 gap-5 p-8">
+        <h2 class="font-600 text-3xl">Rejoindre MidiFriendly</h2>
+        <p>Le moment le plus important d’une journée de travail n’est pas la réunion, mais bien le déjeuner. Echanger, intégrer et rigoler, c’est la période à privilégier pour vous et votre entreprise. Fini la prise de tête à demander où manger, MidiFriendly</p>
+        <NButton type="primary" class="w-min">Demander une démo</NButton>
+      </div>
+      <div class="absolute -right-10 top-10">
+        <n-image
+          width="450"
+          src="http://localhost:3000/_nuxt/assets/images/homepage/join-us.png"
+          preview-disabled
+        />
+      </div>
+      <div class="h-100 w-100 image-container absolute right-0 top-0"></div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
