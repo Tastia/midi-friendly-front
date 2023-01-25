254aa64e5a6a611991798b639bc5369617247322
Mathis Angibaud <mathisangibaud@macbook-pro-de-mathis.home>
2023-01-25 17:31:00 +0100

    feat(homepage): add image first section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="flex overflow-hidden p-18 gap-4 h-4/5 items-center">
+  <div
+    id="first-section"
+    class="flex overflow-hidden p-18 gap-4 h-4/5 items-center"
+  >
     <div class="w-full lg:w-1/2 flex flex-col gap-8">
       <h1 class="font-600 text-5xl">Enfin un moyen pour vous et vos collègues de <span class="text-primary">trouver à manger</span></h1>
       <div class="flex gap-4">
@@ -7,5 +10,16 @@
         <NButton type="secondary">Détails de la réservation</NButton>
       </div>
     </div>
+    <div class="w-full h-5/6 lg:w-1/2 image-container rounded-md"></div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+#first-section {
+  .image-container {
+    background: url("@/assets/images/homepage/midi-friendly-group.jpg") no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+}
+</style>
