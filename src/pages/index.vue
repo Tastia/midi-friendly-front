753efb115bc84aa50e2206476ad480d0012c6d00
Mathis Angibaud <mathisangibaud@macbook-pro-de-mathis.home>
2023-01-25 18:11:25 +0100

    feat(homepage): add manage section without function

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -20,6 +20,22 @@
       <p>Le moment le plus important d’une journée de travail n’est pas la réunion, mais bien le déjeuner. Echanger, intégrer et rigoler, c’est la période à privilégier pour vous et votre entreprise. Fini la prise de tête à demander où manger, MidiFriendly.</p>
     </div>
   </div>
+
+  <div id="manage" class="flex overflow-hidden p-18 gap-12 h-4/5 items-center">
+    <div class="w-full lg:w-1/2 flex flex-col gap-8">
+      <h2 class="font-600 text-3xl">Un outil de gestion</h2>
+      <div class="flex flex-col gap-4">
+        <div class="flex flex-col gap-2">
+          <h3 class="text-primary font-600 text-xl">Favoris</h3>
+          <p>Vous cherchez chaque jour vers quel restaurant se tourner ? Ajouter ses restaurants favoris et pouvoir, d’un coup d’oeil, connaître les goûts de chacun.</p>
+        </div>
+        <h3 class="font-600 text-xl">Groupes</h3>
+        <h3 class="font-600 text-xl">Organisation</h3>
+        <h3 class="font-600 text-xl">Canal d'entreprise</h3>
+      </div>
+    </div>
+    <div class="w-full h-5/6 lg:w-1/2 image-container rounded-md"></div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -33,6 +49,13 @@
 #about {
   .image-container {
     background: url("@/assets/images/homepage/about.jpg") no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+}
+#manage {
+  .image-container {
+    background: url("@/assets/images/homepage/manage.jpg") no-repeat;
     background-size: cover;
     background-position: center;
   }
